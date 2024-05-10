823f8c2d1ebddf378dc2f59be3718d58a4e21489
Noman <nomander17@gmail.com>
2024-05-10 20:55:51 +0530

    Password eye toggle

--- src/pages/Landing/RegistrationForm.jsx
+++ src/pages/Landing/RegistrationForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import request from "../../axiosHelper";
 import Notification from "../../components/Notifications";
+import { Eye, EyeOff } from 'lucide-react';
 
 export const RegistrationForm = ({ setCurrentForm, notification, setNotification }) => {
   const [formData, setFormData] = useState({
@@ -9,6 +10,12 @@ export const RegistrationForm = ({ setCurrentForm, notification, setNotification
     email: "",
     password: "",
   });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -85,7 +92,7 @@ export const RegistrationForm = ({ setCurrentForm, notification, setNotification
         <div className="relative">
           <input
             className="p-2 rounded-xl border w-full text-stone-700"
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             placeholder="Password"
             value={formData.password}
@@ -94,18 +101,13 @@ export const RegistrationForm = ({ setCurrentForm, notification, setNotification
             }
             required
           />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={16}
-            height={16}
-            fill="gray"
-            className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2"
-            viewBox="0 0 16 16"
-            onClick={() => alert("Password toggle req")}
+          <button 
+            type="button" 
+            onClick={togglePasswordVisibility}
+            className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
           >
-            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-          </svg>
+            {showPassword ? <EyeOff size={16} color="gray" /> : <Eye size={16} color="gray" />}
+          </button>
         </div>
         <button className="bg-accent rounded-xl text-zinc-50 py-2 hover:scale-105 duration-300">
           Register
