8d19d711ec3e38b10fa0d41fa79d8ee7fede2490
Noman <nomander17@gmail.com>
2024-05-11 18:48:16 +0530

    Commenting

---
+++ src/pages/Comments/Comment.jsx
@@ -0,0 +1,174 @@
+import { Heart, Trash } from "lucide-react";
+import { useEffect, useState } from "react";
+import { absoluteTime, relativeTime } from "../Home/timeFormat";
+import request from "../../axiosHelper";
+
+// Current userId is the user currently logged in
+// import jwt_decode from "jwt-decode";
+
+// const token = localStorage.getItem('token');
+// const currentUser = token ? jwt_decode(token) : null;
+
+// for now it is 1
+const currentUser = {
+    userId: 2,
+    username: "Admin",
+  };
+  
+
+const Comment = ({ commentId, postId, author, content, timeFormat }) => {
+  const [comment, setComment] = useState({
+    user: {
+      name: "",
+      username: "",
+      avatar: "NO ICON",
+    },
+    likes: [],
+    text: "",
+    time: ""
+  });
+
+  const [liked, setLiked] = useState(false);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const response = await request.get(`/posts/${postId}/comments/${commentId}`);
+        setComment(response.data);
+        setLiked(alreadyLiked(response.data.likes));
+      } catch (error) {
+        console.error(`Error fetching post ${postId}: `, error);
+      }
+    };
+
+    fetchComments();
+  }, [commentId]);
+
+  const alreadyLiked = (likes) => {
+    return likes.some((like) => like.user.userId === currentUser.userId);
+  };
+
+  const getProfileImage = () => {
+    if (author.avatar) {
+      return `data:image/jpeg;base64,${comment.user.avatar}`;
+    } else {
+      const nameParams = author.name.split(" ").join("+");
+      return `https://ui-avatars.com/api/?name=${nameParams}&background=random`;
+    }
+  };
+
+  const handleLike = async () => {
+    console.log("Like button clicked for comment ", commentId);
+    if (liked) {
+      // Unliking comment
+      const likeId = comment.likes.find(
+        (like) => like.user.userId === currentUser.userId
+      )?.likeId;
+      try {
+        await request.delete(`/posts/${postId}/comments/${comment.commentId}/likes/${likeId}`);
+        setLiked(false);
+        // Update comment likes by filtering out the current user's like
+        setComment({
+          ...comment,
+          likes: comment.likes.filter(
+            (like) => like.user.userId !== currentUser.userId
+          ),
+        });
+      } catch (error) {
+        console.error(`Error unliking comment ${commentId} `, error);
+      }
+    } else {
+      // Liking comment
+      try {
+        const response = await request.post(`/posts/${postId}/comments/${comment.commentId}/likes`, {
+          userId: currentUser.userId,
+          commentId: comment.commentId,
+        });
+        setLiked(true);
+        // Update comment likes by adding the new like
+        setComment({
+          ...comment,
+          likes: [...comment.likes, response.data],
+        });
+      } catch (error) {
+        if (error.response && error.response.status === 409) {
+          setLiked(true);
+        } else {
+          console.error(`Error liking comment ${commentId} `, error);
+        }
+      }
+    }
+  };
+
+  const handleDelete = () => {
+    console.log("delete pressed");
+  }
+
+  return (
+    <div className="bg-[#384754] shadow-md rounded-lg p-4 mb-4">
+      <div className="flex items-center">
+        <div className="flex-shrink-0">
+          <img
+            className="inline-block h-10 w-10 rounded-full"
+            src={getProfileImage()}
+            alt=""
+          />
+        </div>
+        <div className="ml-3">
+          <p className="text-base leading-6 font-medium text-white">
+            {author.name}
+            <span className="text-sm leading-5 font-medium text-gray-400 ml-1">
+              @{author.username} -{" "}
+              {timeFormat === "relative"
+                ? relativeTime(comment.time)
+                : absoluteTime(comment.time)}
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="mt-4">
+        <div className="text-base font-medium text-offwhite whitespace-pre-wrap text-left">
+          {comment.text}
+        </div>
+        {comment.image && (
+          <div className="mt-4">
+            <img
+              className="rounded-lg w-full max-h-96 object-contain"
+              src={`data:image/jpeg;base64,${comment.image}`}
+              alt="comment"
+            />
+          </div>
+        )}
+        {/* Interactions */}
+        <div className="flex justify-start items-center mt-4">
+          <button
+            className="flex mr-5 items-center justify-center px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300 ease-in-out"
+            onClick={handleLike}
+          >
+            {liked ? (
+              <Heart
+                fill="red"
+                strokeWidth={0}
+                className={`mr-2 ${liked ? "animate-pop" : ""}`}
+              />
+            ) : (
+              <Heart className="mr-2" />
+            )}
+            Like
+          </button>
+          {comment.user.userId === currentUser.userId && (
+            <button
+              className="flex items-center justify-center px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-700 transition duration-300 ease-in-out"
+              onClick={handleDelete}
+            >
+              <Trash className="mr-2" />
+              Delete
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
---
+++ src/pages/Comments/CreateComment.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import request from "../../axiosHelper";
+import autosize from "autosize";
+
+const currentUser = {
+    userId: 2,
+}
+
+const CreateComment = ({ postId, setComments }) => {
+  const [content, setContent] = useState("");
+
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const commentDTO = {
+        userId: currentUser.userId,
+        postId: postId,
+        text: content
+    };
+    try {
+        await request.post(`/posts/${postId}/comments`, commentDTO);
+        const response = await request.get(`/posts/${postId}/comments`);
+        setComments(response.data);
+        setContent("");
+    } catch (error) {
+        console.error("Error posting comment", error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="bg-[#384754] shadow-md mt-5 rounded-lg p-4">
+        <div className="flex items-center">
+          <div className="mr-4">
+            <img
+              className="inline-block h-12 w-12 rounded-full"
+              src="https://ui-avatars.com/api/name=John+Doe"
+              alt=""
+            />
+          </div>
+          <div className="flex-1">
+            <textarea
+              className="bg-transparent p-3 text-offwhite font-medium text-lg w-full focus:outline-none focus:ring-2 focus:ring-accent rounded-lg"
+              value={content}
+              onChange={handleContentChange}
+              placeholder="Add a comment..."
+              ref={(textarea) => autosize(textarea)}
+              style={{ maxHeight: "200px" }} // Set the maximum height limit
+            ></textarea>
+          </div>
+          {/* Move the button next to the textarea */}
+          <button className="bg-blue-600 hover:bg-white hover:text-blue-600 text-white font-bold py-2 px-4 rounded-full flex items-center transition duration-300 ml-4">
+            <span className="text-sm font-medium">Comment</span>
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CreateComment;
