9ae11ca4aa11e31b991ce420231348a2ab52691f
Noman <nomander17@gmail.com>
2024-05-12 14:15:58 +0530

    focus invalid:focus for forms

--- src/pages/Landing/RegistrationForm.jsx
+++ src/pages/Landing/RegistrationForm.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import request from "../../axiosHelper";
 import Notification from "../../components/Notifications";
-import { Eye, EyeOff } from 'lucide-react';
+import { Eye, EyeOff } from "lucide-react";
 
-export const RegistrationForm = ({ setCurrentForm, notification, setNotification }) => {
+export const RegistrationForm = ({
+  setCurrentForm,
+  notification,
+  setNotification,
+}) => {
   const [formData, setFormData] = useState({
     username: "",
-    name: "", 
+    name: "",
     email: "",
     password: "",
   });
@@ -15,6 +19,35 @@ export const RegistrationForm = ({ setCurrentForm, notification, setNotification
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
+  };
+
+  const handlePasswordChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const validatePassword = (e) => {
+      const password = e.target.value;
+      if (password.length < 8) {
+        e.target.setCustomValidity(
+          "Password must be at least 8 characters long."
+        );
+      } else {
+        e.target.setCustomValidity("");
+      }
+    };
+    validatePassword(e);
+  };
+
+  const handleEmailChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const validateEmail = (e) => {
+      const email = e.target.value;
+      const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+      if (!emailRegex.test(email)) {
+        e.target.setCustomValidity("Please enter a valid email address.");
+      } else {
+        e.target.setCustomValidity("");
+      }
+    };
+    validateEmail(e);
   };
 
   const handleSubmit = async (e) => {
@@ -26,15 +59,15 @@ export const RegistrationForm = ({ setCurrentForm, notification, setNotification
         handleRegisterSuccess(response);
       } else if (response.status === 400) {
         setNotification({
-          "category": "error",
-          "content": response.data
+          category: "error",
+          content: response.data,
         });
       }
     } catch (error) {
       console.log(error);
       setNotification({
-        "category": "error",
-        "content": error.response.data
+        category: "error",
+        content: error.response.data,
       });
     }
   };
@@ -44,20 +77,23 @@ export const RegistrationForm = ({ setCurrentForm, notification, setNotification
     setCurrentForm("login");
     setNotification({
       category: "success",
-      content: "Registration successful. Please login."
+      content: "Registration successful. Please login.",
     });
   };
 
   return (
     <div className="md:w-1/2 px-8 md:px-16 text-zinc-50">
-      { notification && (
-        <Notification content={notification.content} category={notification.category} />
+      {notification && (
+        <Notification
+          content={notification.content}
+          category={notification.category}
+        />
       )}
       <h2 className="font-bold text-2xl">Register</h2>
       <p className="text-xs mt-4">Register as a new user</p>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
-          className="p-2 mt-8 rounded-xl border text-stone-700"
+          className="p-2 mt-8 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 text-stone-700"
           type="text"
           name="username"
           placeholder="Username"
@@ -68,7 +104,7 @@ export const RegistrationForm = ({ setCurrentForm, notification, setNotification
           required
         />
         <input
-          className="p-2 rounded-xl border text-stone-700"
+          className="p-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 text-stone-700"
           type="text"
           name="name"
           placeholder="Name"
@@ -79,34 +115,38 @@ export const RegistrationForm = ({ setCurrentForm, notification, setNotification
           required
         />
         <input
-          className="p-2 rounded-xl border text-stone-700"
+          className="p-2 text-stone-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 invalid:focus:ring-red-600"
           type="email"
           name="email"
           placeholder="Email"
           value={formData.email}
           onChange={(e) =>
-            setFormData({ ...formData, [e.target.name]: e.target.value })
+            handleEmailChange(e)
           }
           required
         />
         <div className="relative">
           <input
-            className="p-2 rounded-xl border w-full text-stone-700"
+            className="p-2 rounded-xl w-full text-stone-700  focus:outline-none focus:ring-2 focus:ring-blue-500 invalid:focus:ring-red-600"
             type={showPassword ? "text" : "password"}
             name="password"
             placeholder="Password"
             value={formData.password}
             onChange={(e) =>
-              setFormData({ ...formData, [e.target.name]: e.target.value })
+              handlePasswordChange(e)
             }
             required
           />
-          <button 
-            type="button" 
+          <button
+            type="button"
             onClick={togglePasswordVisibility}
             className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
           >
-            {showPassword ? <EyeOff size={16} color="gray" /> : <Eye size={16} color="gray" />}
+            {showPassword ? (
+              <EyeOff size={16} color="gray" />
+            ) : (
+              <Eye size={16} color="gray" />
+            )}
           </button>
         </div>
         <button className="bg-accent rounded-xl text-zinc-50 py-2 hover:scale-105 duration-300">
