3523ad3469a11e48f1135b98fd1725b2acf1d2ce
Noman <nomander17@gmail.com>
2024-05-14 15:31:48 +0530

    Better responsive sidebar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,33 @@ import { Feed } from "./Feed";
 import HomeSideBar from "./HomeSideBar";
 import BottomNavBar from "../../components/BottomNavBar";
 import useAuthUser from "react-auth-kit/hooks/useAuthUser";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const HIDE_SIDEBAR_WIDTH = 886;
+  const [isSideBarVisible, setIsSideBarVisible] = useState(
+    window.innerWidth > HIDE_SIDEBAR_WIDTH
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSideBarVisible(window.innerWidth >= 866);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="flex flex-col md:flex-row max-w-screen max-h-screen">
-      <div className="hidden md:block">
-        <HomeSideBar />
-      </div>
+      {isSideBarVisible ? (
+        <div className="block">
+          <HomeSideBar />
+        </div>
+      ) : (
+        <></>
+      )}
       <div className="bg-foreground flex-1 h-screen md:w-3/4">
         {/* panel container to stop flow */}
         <div className="overflow-auto h-screen overflow-x-hidden pb-16 md:pb-0">
@@ -16,9 +36,13 @@ const Home = () => {
         </div>
       </div>
       {/* bottom navbar for small screens */}
-      <div className="md:hidden fixed inset-x-0 bottom-0">
-        <BottomNavBar />
-      </div>
+      {!isSideBarVisible ? (
+        <div className="fixed inset-x-0 bottom-0">
+          <BottomNavBar />
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
